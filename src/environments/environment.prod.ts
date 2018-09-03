f5cfd776348fb004516df2f797467262828cb483
AVIJEET-PC\Avijeet <adyslg133@gmail.com>
2018-09-03 21:09:53 +0530

    updates to deploy to heroku again 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyATAuj28xrq4U_H7yopD1CRaeyhFPJPkLU',
+   authDomain: 'datas-98caa.firebaseapp.com',
+   databaseURL: 'https://datas-98caa.firebaseio.com',
+   projectId: 'datas-98caa',
+   storageBucket: 'datas-98caa.appspot.com',
+   messagingSenderId: '278415361737' 
+ }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase : {
    apiKey: 'AIzaSyATAuj28xrq4U_H7yopD1CRaeyhFPJPkLU',
   authDomain: 'datas-98caa.firebaseapp.com',
